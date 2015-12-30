abab1a5df1cf72924a123dfdec5b247233766851
zhangsuixin <23235140@qq.com>
2015-12-30 10:28:16 +0800

    Initial commit

---
+++ src/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package example;/**
+ * Created by Administrator on 2015/12/30 0030.
+ */
+public class HelloWorld {
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+}
